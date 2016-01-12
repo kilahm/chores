b99ae386b15d0a269436bf9a59522d53d7035a0b
Isaac Leinweber <isaac.leinweber@engrade.com>
2016-01-11 20:14:30 -0800

    secure migration endpoint

--- src/handler/Migrate.php
+++ src/handler/Migrate.php
@@ -3,6 +3,8 @@
 namespace kilahm\chores\handler;
 
 use DateTime;
+use kilahm\chores\Config;
+use kilahm\chores\service\Request;
 use kilahm\IOC\FactoryContainer;
 
 final class Migrate
@@ -10,6 +12,7 @@ final class Migrate
     <<route('get', '/migrate')>>
     public static function showMigrations(FactoryContainer $c, Vector<string> $matches) : void
     {
+        self::checkAuth($c->getRequest(), $c->getConfig());
         $migrator = $c->getMigrator();
         $view = new \Migrate($migrator->listAllMigrations());
         $view->show();
@@ -18,11 +21,21 @@ final class Migrate
     <<route('post', '/migrate')>>
     public static function runMigrations(FactoryContainer $c, Vector<string> $matches) : void
     {
+        self::checkAuth($c->getRequest(), $c->getConfig());
         $migrator = $c->getMigrator();
         $thisRun = $migrator->run();
         $view = new \Migrate($migrator->listAllMigrations(), $thisRun);
         $view->show();
     }
 
+    private static function checkAuth(Request $req, Config $config) : void
+    {
+        $key = (string)$req->header('chores-migration-key');
+        if(password_verify($key, $config->migrationKey())) {
+            return;
+        }
 
+        http_response_code(404);
+        exit();
+    }
 }
